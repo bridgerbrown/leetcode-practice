674d85485481d8a2cc8553da193f795aa5d3ffe8
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-06-07 20:06:43 -0700

    finished the strings problems section

--- top-interview-questions/strings.js
+++ top-interview-questions/strings.js
@@ -140,3 +140,102 @@ var isPalindrome = function (s) {
   }
   return true;
 };
+
+/*
+#6: "String to Integer (atoi)"
+
+Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+The algorithm for myAtoi(string s) is as follows:
+
+Read in and ignore any leading whitespace.
+Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+Return the integer as the final result.
+Note:
+
+Only the space character ' ' is considered a whitespace character.
+Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
+ 
+
+Example 1:
+
+Input: s = "42"
+Output: 42
+Explanation: The underlined characters are what is read in, the caret is the current reader position.
+Step 1: "42" (no characters read because there is no leading whitespace)
+         ^
+Step 2: "42" (no characters read because there is neither a '-' nor '+')
+         ^
+Step 3: "42" ("42" is read in)
+           ^
+The parsed integer is 42.
+Since 42 is in the range [-231, 231 - 1], the final result is 42.
+*/
+
+var myAtoi = function (s) {
+  return Math.max(-(2 ** 31), Math.min(2 ** 31 - 1, parseInt(s) || 0));
+};
+
+/*
+I had a much more inefficient complex answer than this, but I saw this solution from someone else
+and had to learn how it worked. A lot of answers were much much longer, and the simplicity of this one
+is amazing. After looking into it, it really shows the power of parseInt(). It takes care of the whitespace,
+the sign, the converting, and the end of the digit input. Then I learned how you can get a clamped
+answer using the Math.min and Math.max, by having it return between the integers you give. Also,
+parseint will return 'NaN' if no number in input, but the or operator to zero handles that case.
+All in all, handling the whole problem with one line of code is awesome. 
+
+It does take 92ms, so it may not be the most memory efficient but still.
+*/
+
+/*
+#7: "Implement strStr()"
+
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+*/
+
+var strStr = function (haystack, needle) {
+  if (!needle.length) return 0;
+  if (!haystack.includes(needle)) return -1;
+  return haystack.split(`${needle}`)[0].length;
+};
+
+/*
+#8: "Longest Common Prefix"
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+
+*/
+
+var longestCommonPrefix = function (strs) {
+  if (strs === undefined || strs.length === 0) {
+    return "";
+  }
+
+  return strs.reduce((prev, next) => {
+    let i = 0;
+    while (prev[i] && next[i] && prev[i] === next[i]) i++;
+    return prev.slice(0, i);
+  });
+};
