e69c8da9648069476a60ecad589c43a923726d1a
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-08-22 13:40:01 -0700

    reviewing, cleanup

--- top-interview-questions/arrays.js
+++ top-interview-questions/arrays.js
@@ -44,11 +44,12 @@ overall difference.
 
 /*
 #2:
-Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
+The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 
 Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
-
-Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. 
+The remaining elements of nums are not important as well as the size of nums.
 Return k.
 */
 
@@ -76,13 +77,9 @@ var removeDuplicates = function (nums) {
 
 /*
 In order to change the original nums array, I am going to use a for-loop.
-
 I will set a count variable at 1 to use as my index checker, and increase it for each time through the loop.
-
 Since the array is already in non-decreasing order, I can just check each array item to see if the previous
-
 array item is the same, and if they aren't the same then redeclaring that item with the current index.
-
 Then I will finish by returning count.
 */
 
@@ -169,7 +166,7 @@ var containsDuplicate = function (nums) {
 
 var containsDuplicate = function(nums){
   nums.sort();
-  let duplicates == false;
+  let duplicates = false;
   for(let i = 0; i < nums.length; i++){
     if(nums[i] == nums[i - 1]){
       duplicates = true;
