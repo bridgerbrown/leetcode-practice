e547436c070443e5efc7bf8dd9b0bce607fb4fc2
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-10-03 16:43:09 -0700

    studying arr, ll, strings. moving on to sorting+searching and trees.

--- sandbox.js
+++ sandbox.js
@@ -1,22 +1,92 @@
-function anagramStr(s, t) {
-  if (s.length !== t.length) return false;
-  const arr = new Array(26).fill(0);
-  for (let i = 0; i < s.length; i++) {
-    arr[s.charCodeAt(i) - 97]++;
-    arr[t.charCodeAt(i) - 97]--;
-  }
-  return arr.every(count => count === 0);
+function longestPrefix(strs) {
+  if (!strs.length || strs === undefined) return "";
+  return strs.reduce((prev, next) => {
+    const i = 0;
+    while (prev[i] && next[i] && prev[i] === next[i]) i++;
+    return prev.splice(0, i);
+  })
 }
 
-function reverseLl(head) {
-  let prev = null, cur = head, temp = null;
-  while (cur) {
-    temp = cur.next;
-    cur.next = prev;
-    prev = cur;
-    cur = temp;
+function findSingle(nums) {
+  const set = new Set();
+  for (const num of nums) set.has(num) ? set.delete(num) : set.add(num);
+  return set.values().next().value;
+}
+
+function palindromeLl(head) {
+  let fast = slow = head, temp, prev;
+  while (fast && fast.next) fast = fast.next.next, slow = slow.next;
+  prev = slow, slow = slow.next, prev.next = null
+  while (slow) temp = slow.next, slow.next = prev, prev = slow, slow = temp;
+  fast = head, slow = prev;
+  while (slow) {
+    if (fast.val !== slow.val) return false;
+    fast = fast.next, slow = slow.next;
   }
-  return prev;
+  return true;
+}
+
+function firstUniqueChar(str) {
+  const freq = {};
+  for (const char of str) freq[char] = (freq[char] || 0) + 1;
+  for (let i = 0; i < str.length; i++) {
+    if(freq[str[i]] === 1) return i;
+  }
+}
+
+function strAtoi(str) {
+  return Math.max(-(2 ** 31), Math.min(2 ** 31 - 1, parseInt(str) || 0));
+}
+
+function strStr(haystack, needle) {
+  if (!needle.length) return 0;
+  return haystack.indexOf(needle);
+}
+
+function anagramStr(s, t) {
+  if(s.length !== t.length) return false;
+  const alph = new Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    aplh[s.charCodeAt(i) - 97]++
+    aplh[t.charCodeAt(i) - 97]--
+  }
+  return alph.every(count => count === 0);
+}
+
+function reverseInt(x) {
+  const abs = Math.abs(x).toString().split("").reverse().join();
+  const parsed = parseInt(abs);
+  if (parsed > 2 ** 31) return 0;
+  return parsed * Math.sign(x);
+}
+
+function removeDuplicates(nums) {
+  const count = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] !== nums[i]) {
+      nums[i] = nums[count];
+      count++
+    }
+  }
+  return count;
+}
+
+function palindromStr(str) {
+  str = str.replace(/[^a-b0-9]/gi, "");
+  for (let i = 0, j = str.length - 1; i <= j; i++, j--) {
+    if (str.charAt[i] !== str.charAt[j]) return false
+  }
+  return true;
+}
+
+function reverseStr(arr) {
+  let i = 0, j = arr.length, temp;
+  while (i < j) {
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    i++, j--;
+  }
 }
 
 function longestPrefix(strs) {
@@ -24,21 +94,30 @@ function longestPrefix(strs) {
   return strs.reduce((prev, next) => {
     const i = 0;
     while (prev[i] && next[i] && prev[i] === next[i]) i++;
-    return prev.slice(0, i);
+    return prev(0, i);
   })
 }
 
-function reverseString(s) {
-  let i = 0, j = s.length - 1;
-  while (i < j) {
-    let temp = s[i];
-    s[i] = s[j];
-    s[j] = temp;
-    i++, j--;
-  }
+function reverseInt(x) {
+  const abs = x.toString().split("").reverse().join();
+  const int = parseInt(abs);
+  if (int > 2 ** 31) return 0;
+  return int * Math.sign(x);
 }
 
-function mergeLls(list1, list2) {
+function palindromStr(str) {
+  str = str.toLowerCase().replace(/[^a-b0-9]/gi, "");
+  for (let i = 0, j = str.length - 1; i <= j; i++, j--) {
+    if (str.charAt[i] !== str.charAt[j]) return false;
+  }
+  return true;
+}
+
+function strAtoi(s) {
+  return Math.max(-(2 ** 31), Math.min(2 ** 31 - 1, parseInt(s) || 0));
+}
+
+function mergeLists(list1, list2) {
   const merged = new Node();
   const head = merged;
   while (list1 && list2) {
@@ -54,124 +133,39 @@ function mergeLls(list1, list2) {
   merged.next = list1 || list2;
   return head.next;
 }
-
-function cycleLl(head, pos) {
-  let fast = head;
-  while (fast && fast.next) {
-    fast = fast.next.next;
-    head = head.next;
-    if (fast === head) return true;
+  
+function removeDuplicates(str) {
+  const count = 1;
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] !== str[i]) {
+      str[i] = str[count];
+      count++;
+    }
   }
-  return false;
-}
-
-function palindromStr(str) {
-  str = str.toLowerCase().replace(/[^a-b0-9]/gi, "");
-  for (let i = 0, j = str.length - 1; i <= j; i++, j--) {
-    if (str[i] !== str[j]) return false;
-  }
-  return true;
-}
-
-function removeNthFromList(head, n) {
-  let fast = slow = head;
-  for (let i = 0; i < n; i++) fast = fast.next;
-  if (!fast) return head.next;
-  while (fast && fast.next) fast = fast.next, slow = slow.next;
-  slow.next = slow.next.next;
-  return head;
-}
-
-function stringAtoi(str) {
-  return Math.max(-(2 ** 31), Math.min(2 ** 31 - 1, parseInt(str) || 0));
-}
-
-function firstUniqueChar(str) {
-  const freq = {};
-  for (const char of str) {
-    freq[char] = (freq[char] || 0) + 1;
-  }
-  for (let i = 0; i < str.length; i++) {
-    if (freq[str[i]] === 1) return i; 
-  }
-  return -1;
-}
-
-function longestPrefix(strs) {
-  if (strs === undefined || strs.length === 0) return "";
-  return strs.reduce((prev, next) => {
-    const i = 0;
-    while (prev[i] && next[i] && prev[i] === next[i]) i++;
-    return prev.slice(0, i);
-  })
-}
-
-function reverseInt(x) {
-  const abs = Math.abs(x).split("").reverse().join("")
-  const parsed = parseInt(abs);
-  if (parsed > 2 ** 31) return 0;
-  return parsed * Math.sign(x);
-}
-
-function reverseString(arr) {
-  let i = 0, j = arr.length - 1;
-  while (i < j) {
-    let temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-    i++, j--;
-  }
-}
-
-function firstUniqueChar(str) {
-  const freq = {};
-  for (const char of str) freq[char] = (freq[char] || 0) + 1;
-  for (let i = 0; i < str.length; i++) {
-    if (freq[str[i]] === 1) return i;
-  }
-  return -1;
+  return count;
 }
 
 function strStr(haystack, needle) {
-  if (!needle.length) return -1;
+  if (!needle.length) return 0;
   return haystack.indexOf(needle);
 }
 
 function anagramStr(s, t) {
   if (s.length !== t.length) return false;
-  const chars = new Array(26).fill(0);
+  const alph = new Array(26).fill(0);
   for (let i = 0; i < s.length; i++) {
-    chars[s.charCodeAt(i) - 97]++;
-    chars[t.charCodeAt(i) - 97]--;
+    alph[s.charCodeAt(i) - 97]++;
+    alph[t.charCodeAt(i) - 97]--;
   }
-  return chars.every(count => count === 0);
+  return alph.every(count => count === 0);
 }
 
-function palindromStr(str) {
-  str = str.toLowerCase().replace(/[^a-b0-9]/gi, "");
-  for (let i = 0, j = str.length - 1; i <= j; i++, j--) {
-    if (str[i] !== str[j]) return false;
+function firstUniqueChar(str) {
+  const freq = {};
+  for (let char of str) {
+    freq[char] = (freq[char] || 0) + 1;
   }
-  return true;
-}
-
-function strAtoi(str) {
-  return Math.max(-(2 ** 31), Math.min(2 ** 31 - 1, parseInt(str) || 0));
-}
-
-function deleteNode(head, node) {
-  let temp = node.next;
-  node.val = temp.val;
-  node.next = temp.next;
-}
-
-function incrementDigitsArray(digits) {
-  const carry = 1;
-  for (let i = digits.length - 1; i >= 0; i--) {
-    digits[i] += carry;
-    carry = Math.floor(digits[i] / carry);
-    digits[i] %= 10;
+  for (let i = 0; i < str.length; i++) {
+    if (freq[str[i]] === 1) return i;
   }
-  if (carry) digits.unshift(carry);
-  return digits;
 }
