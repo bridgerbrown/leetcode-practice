bd1489d0a71f7b11be61366525e52b2b296c7238
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-10-09 13:20:35 -0700

    trees, sorting, searching, etc. practice

--- sandbox.js
+++ sandbox.js
@@ -1,102 +1,73 @@
-function validateBST(root) {
-  if (!root) return true;
-  const validate = (root, min, max) => {
-    if (!root) return true;
-    if ((min !== null && root.val <= min) || (max !== null && root.val >= max)) return false;
-    validate(root.left, min, root.val), validate(root.right, root.val, max);
-  }
-  return validate(root, null, null);
-};
-
-function BTlevelTraversal(root) {
-  const results = [];
-  if (!root) return results;
-  const DFS = (node, level) => {
-    results[count] = [...(results[count] || []), node.val];
-    if (node.left) DFS(node.left, level + 1);
-    if (node.right) DFS(node.right, level + 1);
-  };
-  DFS(root, 0);
-  return results;
-}
-
-function strStr(haystack, needle) {
-  if (!needle.length) return 0;
-  return haystack.indexOf(needle);
-}
-
 function convertArrToBST(nums) {
   const BST = (left, right) => {
     if (left > right) return null;
     const mid = Math.floor((left + right) / 2);
-    const cur = new TreeNode(nums[mid]);
-    cur.left = BST(left, mid - 1);
-    cur.right = BST(mid + 1, right);
-    return cur;
+    const current = new TreeNode(nums[mid]);
+    current.left = BST(left, mid - 1);
+    current.right = BST(mid + 1, right);
+    return current;
   };
   return BST(0, nums.length - 1);
 }
 
 function firstBadVersion(isBadVersion) {
-  return function(n) {
+  return function (n) {
     let left = 0, right = n;
     while (right - left !== 1) {
       let mid = parseInt((left + right) / 2);
       isBadVersion(mid) ? right = mid : left = mid;
     }
+    return right;
+  }
+}
+
+function validateBST(root) {
+  if (!root) return true;
+  const validate = (root, min, max) => {
+    if (!root) return true;
+    if ((min !== null && root.val <= min) || (max !== null && root.val >= max)) return false;
+    return validate(root.left, min, root.val) && validate(root.right, root.val, max);
+  }
+  return validate(root, null, null);
+}
+
+function BTlevelTraversal(root) {
+  const results = [];
+  if (!root) return results;
+  const DFS = (cur, level) => {
+    results[level] = [...(results[level], []), cur.val];
+    if (cur.left) DFS(cur.left, level + 1)
+    if (cur.right) DFS(cur.right, level + 1);
   };
+  DFS(root, 0);
+  return results;
 }
 
 function symmetricTree(root) {
   if (!root) return true;
-  const DFS = (left, right) => {
-    if (left && !right || !left && right || left.val !== right.val) return false;
-    DFS(left.right, right.left), DFS(left.left, right.right);    
+  const check = (left, right) => {
+    if (!left && !right) return true;
+    if (!left && right || left && !right || left.val !== right.val) return false;
+    check(left.right, right.left), check(left.left, right.right);
   }
-  return DFS(root.left, root.right)
+  return check(root.left, root.right);
 }
 
-function removeDuplicates(nums, k) {
-  const count = 1;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i - 1] !== nums[i]) {
-      nums[count] = nums[i];
-      count++
-    }
-  }
-  return count;
+function reverseInt(x) {
+  const rev = Math.abs(x).toString().split("").reverse().join();
+  const int = parseInt(rev);
+  if (int > 2 ** 31 - 1) return 0;
+  return int * Math.sign(x);
 }
 
-function mergeSortedArr(nums1, m, nums2, n) {
-  let i = m - 1, j = n - 1, k = m + n - 1;
-
-  while (i >= 0 && j >= 0) {
-    if (nums1[i] > nums2[j]) {
-      nums1[k] = nums1[i];
-      i--, k--;
-    } else {
-      nums1[k] = nums2[i];
-      j--, k--;
-    }
+function twoSum(nums, target) {
+  const differences = {};
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+    if (diff in differences) return [i, differences[diff]];
+    differences[nums[i]] = i;
   }
-
-  while (j >= 0) {
-    nums1[k] = nums2[j];
-    j--, k--;
-  }
-  return nums1;
-}
-
-function firstUniqueChar(str) {
-  const freq = {};
-  for (let char of str) {
-    freq[char] = (freq[char] || 0) + 1;
-  }
-  for (let i = 0; i < str.length; i++) {
-    if (freq[str[i]] == 1) return i;
-  }
-  return -1;
-}
+};
 
 function maxDepth(root) {
   if (!root) return null;
@@ -104,58 +75,11 @@ function maxDepth(root) {
   return max + 1;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-function maxDepth(root) {
-  if (!root) return null;
-  let max =Math.max(maxDepth(root.left), maxDepth(root.right));
-  return max + 1
-}
-
-function symmetricTree(root) {
-  if (!root) return true;
-  const DFS = (left, right) => {
-    if (!left && !right) return true;
-    if (left && !right || !left && right || left.val !== right.val) return false;
-    DFS(left.right, right.left), DFS(left.left, right.right);
-  }
-  return DFS(root.left, root.right);
-}
-
-function anagramStr(s, t) {
-  if (s.length !== t.length) return false;
-  const alph = new Array(26).fill(0);
-  for (let i = 0; i < s.length; i++) {
-    alph[s.charCodeAt(i) - 97]++;
-    alph[t.charCodeAt(i) - 97]--;
-  }
-  return alph.every(count => count === 0);
-}
-
-function validateBST(root) {
-  if (!root) return true;
-  const check = (root, min, max) => {
-    if ((min !== null && min.val >= root.val) || (max !== null && max.val <= root.val)) return false;
-    check(root.left, min, root), check(root.right, root, max);    
-  }
-  return check(root, null, null);
-}
-
 function mergeSortedArr(nums1, m, nums2, n) {
   let i = m - 1, j = n - 1, k = m + n - 1;
+
   while (i >= 0 && j >= 0) {
-    if (nums1[i] > nums2[j]) {
+    if (nums1[i].val > nums2[j].val) {
       nums1[k] = nums1[i];
       i--;
     } else {
@@ -164,6 +88,7 @@ function mergeSortedArr(nums1, m, nums2, n) {
     }
     k--;
   }
+
   while (j >= 0) {
     nums1[k] = nums2[j];
     j--, k--;
@@ -171,46 +96,12 @@ function mergeSortedArr(nums1, m, nums2, n) {
   return nums1;
 }
 
-function palindrom(str) {
+function palindromStr(str) {
   const chars = str.toLowerCase().replace(/[^a-b0-9]/gi, "");
-  for (let i = 0, j = str.length - 1; i <= j; i++, j--) {
-    if (chars.charAt(i) !== chars.charAt(j)) return false;
+  for (let i = 0, j = chars.length - 1; i <= j; i++, j--) {
+    if (chars[i] !== chars[j]) return false;
   }
   return true;
-}
-
-function convertArrToBST(nums) {
-  const BST = (left, right) => {
-    if (left > right) return null;
-    const mid = Math.floor((left + right) / 2);
-    const node = new TreeNode(nums[mid]);
-    node.left = BST(left, mid - 1);
-    node.right = BST(mid + 1, right); 
-    return node;
-  }
-  BST(0, nums.length - 1);
-}
-
-function BTlevelTraversal(root) {
-  const results = [];
-  const DFS = (node, level) => {
-    results[level] = [...(results[level] || []), node.val];
-    DFS(node.left, level + 1), DFS(node.right, level + 1);
-    return level;
-  }
-  DFS(root, 0);
-  return results;
-}
-
-function firstUniqueChar(str) {
-  const freq = {};
-  for (let char of str) {
-    freq[char] = (freq[char] || 0) + 1;
-  }
-  for (let i = 0; i < str.length; i++) {
-    if (freq[str[i]] === 1) return i;
-  }
-  return -1;
 }
 
 function firstBadVersion(isBadVersion) {
@@ -220,6 +111,93 @@ function firstBadVersion(isBadVersion) {
       const mid = parseInt((left + right) / 2);
       isBadVersion(mid) ? right = mid : left = mid;
     }
-    return right;
+  };
+}
+
+function palindromStr(str) {
+  str = str.toLowerCase().replace(/[^a-b0-9]/gi, "");
+  for (let i = 0, j = str.length - 1; i <= j; i++, j--) {
+    if (str.charAt(i) !== str.charAt(j)) return false;
   }
+  return true;
+}
+
+function symmetricTree(root) {
+  if (!root) return true;
+  const DFS = (left, right) => {
+    if (!left && !right) return true;
+    if (!left && right || left && !right || left.val !== right.val) return false;
+    DFS(left.right, right.left), DFS(left.left, right.right);
+  }
+  return DFS(root.left, root.right);
+}
+
+function maxDepth(root) {
+  if (!root) return null;
+  let max = Math.max(maxDepth(root.left), maxDepth(root.right));
+  return max + 1;
+}
+
+function longestPrefix(strs) {
+  if (strs === undefined || strs.length === 0) return "";
+  return strs.reduce((prev, next) => {
+    const i = 0;
+    while (prev[i] && next[i] && prev[i] === next[i]) i++;
+    return prev.slice(0, i);
+  });
+};
+
+function BTlevelTraversal(root) {
+  const results = [];
+  if (!root) return results;
+  const DFS = (node, level) => {
+    results[level] = [...(results[level] || []), node.val];
+    if(node.left) DFS(node.left, level + 1);
+    if(node.right) DFS(node.right, level + 1);
+  }
+  DFS(root, 0);
+  return results;
+}
+
+function strAtoi(str) {
+  return Math.max(-(2 ** 31), Math.min(2 ** 31 - 1, parseInt(str) || 0));
+}
+
+function convertArrToBST(nums) {
+  const BST = (left, right) => {
+    if (left > right) return null;
+    const mid = Math.floor((left + right)/ 2);
+    const current = new TreeNode(nums[mid]);
+    current.left = BST(left, mid - 1);
+    current.right = BST(mid + 1, right);
+    return current;
+  }
+  return BST(0, nums.length - 1);
+}
+
+function validateBST(root) {
+  if (!root) return true;
+  const validate = (root, min, max) => {
+    if (!root) return true;
+    if ((min !== null && min >= root) || (max !== null && max <= root)) return false;
+    validate(root.left, min, root), validate(root.right, root, max);    
+  }
+  return validate(root, null, null);
+}
+
+function mergeSortedArr(nums1, m, nums2, n) {
+  let i = m - 1, j = n - 1, k = m + n - 1;
+
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i], i--;
+    } else {
+      nums1[k] = nums2[j], j--;
+    }
+    k--;
+  }
+  while(j >= 0) {
+    nums1[k] = nums2[j], j--, k--;
+  }
+  return nums1;
 }
