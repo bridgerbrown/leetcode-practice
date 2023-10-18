a5b60aec5fd48dca7478c8c76a662013f3a23acf
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-10-18 12:48:28 -0700

    math, design, dynamic, practice

--- sandbox.js
+++ sandbox.js
@@ -1,33 +1,5 @@
-function houseRobber(nums) {
-  let before = prev = 0;
-  for (let i = 0; i < nums.length; i++) {
-    let temp = prev;
-    prev = Math.max(nums[i] + before, prev);
-    before = temp;
-  }
-  return prev;
-}
-
-var Solution = function(nums) {
-  this.nums = nums;
-}
-Solution.prototype.reset = function() {
-  return this.nums;
-}
-Solution.prototype.shuffle = function() {
-  const shuffled = this.nums.slice();
-  const n = shuffled.length;
-  const swap = (arr, i, j) => {
-    let temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
-  }
-  for (let i = 0; i < n; i++) {
-    swap(shuffled, i, Math.floor(Math.random() * n));
-  }
-  return shuffled;
-}
-
 function maxSubarray(nums) {
-  let prev = 0, max = -Infinity;
+  const prev = 0, max = -Infinity;
   for (let i = 0; i < nums.length; i++) {
     prev = Math.max(prev + nums[i], nums[i]);
     max = Math.max(max, prev);
@@ -35,22 +7,8 @@ function maxSubarray(nums) {
   return max;
 }
 
-function countPrimes(n) {
-  let count = 0, primes = [];
-  if (n <= 1) return 0;
-  for (let i = 2; i < n; i++) {
-    if (primes[i] == undefined) {
-      primes[i] = true, count++;
-      for (let j = 2; j * i < n; j++) {
-        primes[i * j] = false;
-      }
-    }
-  }
-  return count;
-}
-
-function bestBuySellStock(prices) {
-  let buy = max = 0, sell = 1;
+function bestStockProfit(prices) {
+  let buy = 0, sell = 1, max = 0;
   while (sell < prices.length) {
     if (prices[buy] < prices[sell]) {
       let profit = prices[sell] - prices[buy];
@@ -61,6 +19,21 @@ function bestBuySellStock(prices) {
     sell++;
   }
   return max;
+}
+
+function countPrimes(n) {
+  let count = 0, primes = [];
+  if (n <= 1) return 0;
+  for (let i = 2; i < n; i++) {
+    if (primes[i] == undefined) {
+      primes[i] = true;
+      count++;
+      for (let j = 2; j * i < n; j++) {
+        primes[i * j] = false;
+      }
+    }
+  }
+  return count;
 }
 
 function climbStairs(n) {
@@ -74,28 +47,71 @@ function climbStairs(n) {
   return current;
 }
 
-function BTLevelTraversal(root) {
-  const results = [];
-  if (!root) return results;
-  const DFS = (node, level) => {
-    results[level] = [...(results[level] || []), node.val];
-    if (node.left) DFS(node.left, level + 1);
-    if (node.right) DFS(node.right, level + 1);
-    return node;
+var MinStack = function() {
+  this.minStack = [];
+  this.main = [];
+}
+MinStack.prototype.push = function(n) {
+  this.main.push(n);
+  if (this.minStack.length === 0 || n <= this.minStack[this.minStack.length - 1]) {
+    this.minStack.push(n);
   }
-  DFS(root, 0);
-  return results;
+}
+MinStack.prototype.pop = function(n) {
+  this.main.pop(n);
+  if (n == this.minStack[this.minStack.length - 1]) this.minStack.pop(n);
+}
+MinStack.prototype.top = function() {
+  return this.main[this.main.length - 1];
+}
+MinStack.prototype.getMin = function() {
+  return this.minStack[this.minStack.length - 1];
+}
+
+function containsDuplicate(nums) {
+  const set = new Set();
+  for (let num of nums) {
+    if (set.has(num)) {
+      return true
+    } else {
+      set.add(num);
+    }
+  }
+  return false;
+}
+
+function twoSum(nums, target) {
+  const differences = {};
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+    if (diff in differences) {
+      return [i, differences[diff]];
+    } else {
+      differences[nums[i]] = i;
+    }
+  }
+}
+
+function houseRobber(nums) {
+  let prev = before = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let temp = prev;
+    prev = Math.max(nums[i] + before, prev);
+    before = temp;
+  }
+  return prev;
 }
 
 function fizzBuzz(n) {
   const results = [];
+  if (n <= 0) return results;
   for (let i = 1; i <= n; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
       results.push("FizzBuzz");
-    } else if (i % 3 == 0) {
+    } else if (i % 3) {
       results.push("Fizz");
-    } else if (i % 5 == 0) {
-      results.push("Buzz");
+    } else if (i % 5) {
+      results.push("Buzz")
     } else {
       results.push(i.toString());
     }
@@ -103,40 +119,147 @@ function fizzBuzz(n) {
   return results;
 }
 
-var MinStack = function() {
-  this.minStack = [];
-  this.container = [];
-};
-MinStack.prototype.push = function(val) {
-  this.container.push(val);
-  if (this.minStack.length === 0 || val <= this.minStack[this.minStack.length - 1]) {
-    this.minStack.push(val);  
+var Solution = function(nums) {
+  this.nums = nums;
+}
+Solution.prototype.reset = function() {
+  return this.nums;
+}
+Solution.prototype.shuffle = function() {
+  const shuffled = this.nums.slice();
+  const n = shuffled.length;
+  const swap = (arr, i, j) => {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
   }
-};
-MinStack.prototype.pop = function() {
-  let val = this.container.pop();
-  if (val === this.minStack[this.minStack.length - 1]) {
-    this.minStack.pop();
+  for (let i = 0; i < n; i++) {
+    swap(shuffled, i, Math.floor(Math.random() * n));
   }
-};
-MinStack.prototype.top = function() {
-  return this.container[this.container.length - 1];
-};
-MinStack.prototype.getMin = function() {
-  return this.minStack[this.minStack.length - 1];
-};
+  return shuffled;
+} 
 
-function mergeSortedLLs(list1, list2) {
-  const merged = new Node();
-  const head = merged;
-  while (list1 && list2) {
-    if (list1.val < list2.val) {
-      merged.next = new Node(list1.val), list1 = list1.next;
+function maxDepth(root) {
+  if (!root) return null;
+  let max = Math.max(maxDepth(root.left), maxDepth(root.right));
+  return max + 1;
+}
+
+function bestStockProfit(prices) {
+  let profit, buy = 0, sell = 1;
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[buy] < prices[sell]) {
+      profit = Math.max(prices[sell] - prices[buy], profit);
     } else {
-      merged.next = new Node(list2.val), list2 = list2.next;
+      buy = sell;
     }
-    merged = merged.next;
+    sell++;
   }
-  merged.next = list1 || list2;
-  return head.next;
+  return profit;
+}
+
+function maxSubarray(nums) {
+  let prev = 0, max = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    prev = Math.max(nums[i] + prev, nums[i]);
+    max = Math.max(max, prev);
+  }
+  return max;
+}
+
+function convertSortedArrToBST(nums) {
+  const BST = (left, right) => {
+    if (left > right) return null;
+    const mid = Math.floor((left + right) / 2);
+    const node = new Node(nums[mid]);
+    node.left = BST(left, mid - 1);
+    node.right = BST(mid + 1, right);
+    return node;
+  }
+  return BST(0, nums.length - 1);
+}
+
+var Solution = function(nums) {
+  this.nums = nums;
+}
+Solution.prototype.reset = function() {
+  return this.nums;
+}
+Solution.prototype.shuffle = function() {
+  const shuffled = this.nums.slice();
+  const n = shuffled.length;
+  const swap = (arr, i, j) => {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  for (let i = 0; i < n; i++) {
+    swap(shuffled, i, Math.floor(Math.random * n));
+  }
+  return shuffled;
+}
+
+function houseRobber(nums) {
+  let before = prev = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let temp = prev;
+    prev = Math.max(nums[i] + before, prev);
+    before = temp;
+  }
+  return prev;
+}
+
+function reverseInt(x) {
+  const abs = Math.abs(x).toString().split("").reverse().join("");
+  const parsed = parseInt(abs);
+  if (parsed > 2 ** 31) return 0;
+  return parsed * Math.sign(x);
+}
+
+function palindromeLL(head) {
+  let fast = slow = head, temp, prev;
+  while (fast && fast.next) {
+    fast = fast.next.next, slow = slow.next;
+  }
+  prev = slow, slow = slow.next, prev.next = null;
+  while (slow) {
+    temp = slow.next;
+    slow.next = prev;
+    prev = slow;
+    slow = temp;
+  }
+  fast = head, slow = prev;
+  while (slow) {
+    if (fast.val !== slow.val) {
+      return false
+    }
+    fast = fast.next, slow = slow.next;
+  }
+  return true;
+}
+
+function climbStairs(n) {
+  if (n <= 2) return n;
+  let first = 1, second = 2, current
+  for (let i = 3; i <= n; i++) {
+    current = first + second;
+    first = second;
+    second = current;
+  }
+  return current;
+}
+
+function countPrimes(n) {
+  let count = 0, primes = [];
+  if (n <= 1) return 0;
+  for (let i = 2; i < n; i++) {
+    if (primes[i] == undefined) {
+      primes[i] = true;
+      count++;
+      for (let j = 2; j * i < n; j++) {
+        primes[i * j] = false;
+      }
+    }
+  }
+  return count;
 }
