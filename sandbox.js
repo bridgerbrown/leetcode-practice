d3b9ffa8e0360b543adb14fc033952e9bca5d654
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-10-27 14:01:05 -0700

    others, bit practice

--- sandbox.js
+++ sandbox.js
@@ -1,35 +1,60 @@
-function pascalsTriangle(numRows) {
-  if (!numRows || numRows <= 0) return [];
-  const pascal = [[1]];
-  for (let i = 1; i < numRows; i++) {
-    const prevRow = pascal[pascal.length - 1];
-    const shiftLeft = [...prevRow, 0];
-    const shiftRight = [0, ...prevRow];
-    const currentRow = shiftLeft.map((r, i) => r + shiftRight[i]);
-    pascal.push(currentRow);
+function missingNumber(nums) {
+  let count = 0, total = 0;
+  for (let i = 0; i < nums.length; i++) {
+    count += nums[i];
+    total += i + 1;
   }
-  return pascal;
+  return total - count;
 }
 
-function romanToInteger(s) {
+function validParentheses(s) {
   const map = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000,
+    ')': '(',
+    '}': '{',
+    ']': '['
   };
-  const results = 0;
+  let arr = [];
   for (let i = 0; i < s.length; i++) {
-    if (map[s[i]] < map[s[i + 1]]) {
-      results -= map[s[i]];
+    if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+      arr.push(s[i]);
     } else {
-      results += map[s[i]];
+      if (arr[arr.length - 1] === map[s[i]]) {
+        arr.pop();
+      } else return false;
     }
   }
-  return results;
+  return arr.length === 0;
+}
+
+function powerOfThree(n) {
+  while (n > 1) {
+    n /= 3;
+  }
+  return n === 1;
+}
+
+function reverseBits(n) {
+  let result = 0, count = 32;
+  while (count--) {
+    result *= 2;
+    result += n & 1;
+    n = n >> 1;
+  }
+  return result;
+}
+
+function countPrimes(n) {
+  let count = 0, primes = [];
+  if (n <= 1) return count;
+  for (let i = 2; i < n; i++) {
+    if (primes[i] == undefined) {
+      primes[i] = true, count++;
+      for (let j = 2; j * i < n; j++) {
+        primes[i * j] = false;
+      }
+    } 
+  }
+  return count;
 }
 
 function hammingDistance(x, y) {
@@ -43,64 +68,77 @@ function reverseBits(n) {
     result += n & 1;
     n = n >> 1;
   }
+  return result;
 }
 
-function numberOf1Bits(n) {
+function maxSubarray(nums) {
+  let prev = 0, max = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    prev = Math.max(nums[i] + prev, nums[i]);
+    max = Math.max(prev, max);
+  }
+  return max;
+}
+
+function pascalsTriangle(numRows) {
+  if (!numRows || numRows <= 0) return [];
+  let pascals = [[1]];
+  for (let i = 0; i < numRows; i++) {
+    const prevRow = pascals[pascals.length - 1];
+    const leftRow = [...prevRow, 0];
+    const rightRow = [0, ...prevRow];
+    const currRow = leftRow.map((r, i) => r + rightRow[i]);
+    pascals.push(currRow);
+  }
+  return pascals;
+}
+
+function romanToInteger(s) {
+  let symb = {
+    ...
+  }
   let count = 0;
-  while (n != 0) {
-    count += n & 1;
-    n = n >>> 1;
+  for (let i = 0; i < s.length; i++) {
+    if (symb[s[i]] < symb[s[i + 1]]) {
+      count -= symb[s[i]];
+    } else {
+      count += symb[s[i]];
+    }
   }
   return count;
 }
 
 function numberOf1Bits(n) {
   let count = 0;
-  while (n != 0) {
+  while (n !== 0) {
     count += n & 1;
     n = n >>> 1;
   }
   return count;
 }
 
-function missingNumber(nums) {
-  let sum = total = 0;
+function houseRobber(nums) {
+  let before = prev = 0, curr;
   for (let i = 0; i < nums.length; i++) {
-    sum += nums[i];
-    total += i + 1;
-  }
-  return total - sum;
-}
-
-function powerOfThree(n) {
-  while (n > 1) {
-    n /= 3;
-  }
-  return n === 1;
-}
-
-function houseRobber(nums){
-  let prev = before = 0, temp;
-  for (let i = 0; i < nums.length; i++) {
-    temp = prev; 
+    curr = prev;
     prev = Math.max(nums[i] + before, prev);
-    before = temp;
+    before = curr;
   }
   return prev;
 }
 
+function hammingDistance(x, y) {
+  return (x ^ y).toString(2).replace(/0/g, '').length;
+}
+
 function validParentheses(s) {
-  const p = {
-    ')' : '(',
-    '}' : '{',
-    ']' : '[',
-  };
+  const chars = {...};
   let arr = [];
   for (let i = 0; i < s.length; i++) {
-    if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+    if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
       arr.push(s[i]);
     } else {
-      if (arr[arr.length - 1] === map[s[i]]) {
+      if (arr[arr.length - 1] == chars[s[i]]) {
         arr.pop();
       } else return false;
     }
@@ -108,48 +146,12 @@ function validParentheses(s) {
   return arr.length === 0;
 }
 
-function hammingDistance(x, y) {
-  return (x ^ y).toString(2).replace(/0/g, '').length;
-}
-
 function reverseBits(n) {
-  let result = 0, count = 32; 
+  let res = 0, count = 32;
   while (count--) {
-    result *= 2;
-    result += n & 1;
-    n = n >> 1;
+    res *= 2;
+    res += n & i;
+    n >> 1;
   }
-  return result;
-}
-
-function houseRobber(nums) {
-  let before = prev = 0, temp;
-  for (let i = 0; i < nums.length; i++) {
-    temp = prev;
-    prev = Math.max(nums[i] + before, prev);
-    before = temp;
-  }
-  return prev;
-}
-
-function pascalsTriangle(numRows) {
-  if (!numRows || numRows <= 0) return [];
-  const pascal = [[1]];
-  for (let i = 1; i < numRows; i++) {
-    const prevRow = pascal[pascal.length - 1];
-    const shiftLeft = [...prevRow, 0];
-    const shiftRight = [0, ...prevRow];
-    const currentRow = shiftLeft.map((r, i) => r + shiftRight[i]);
-    pascal.push(currentRow);
-  }
-  return pascal;
-}
-
-function missingNumber(nums) {
-  let sum = total = 0;
-  for (let i = 0; i < nums.length; i++) {
-    sum += nums[i];
-    total += i + 1;
-  }
-  return total - sum;
+  return res;
 }
