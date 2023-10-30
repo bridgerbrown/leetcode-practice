777221cf04e2148ede2b855f75f3e0ee044a2410
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-10-30 12:43:23 -0700

    math, bits, etc practice

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,44 @@
-function missingNumber(nums) {
-  let count = 0, total = 0;
-  for (let i = 0; i < nums.length; i++) {
-    count += nums[i];
-    total += i + 1;
+function reverseBits(n) {
+  let rev = 0, count = 32;
+  while (count--) {
+    rev *= 2;
+    rev += n & 1;
+    n >> 1;
   }
-  return total - count;
+  return rev;
 }
 
-function validParentheses(s) {
-  const map = {
-    ')': '(',
-    '}': '{',
-    ']': '['
-  };
-  let arr = [];
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
-      arr.push(s[i]);
-    } else {
-      if (arr[arr.length - 1] === map[s[i]]) {
-        arr.pop();
-      } else return false;
-    }
+function numberOf1Bits(n) {
+  let count = 0;
+  while (n !== 0) {
+    count += n & 1;
+    n = n >>> 1;
+  } 
+  return sum;
+}
+
+function pascalsTriangle(numRows) {
+  if (!numRows || numRows <= 0) return [];
+  let pascals = [[1]];
+  for (let i = 1; i < numRows; i++) {
+    let prevRow = pascals[pascals.length - 1];
+    let leftRow = [...pascals, 0];
+    let rightRow = [0, ...pascals];
+    let currRow = leftRow.map((r, i) => r += rightRow[i]);
+    pascals.push(currRow);
   }
-  return arr.length === 0;
+  return pascals;
+}
+
+function climbStairs(n) {
+  let first = 1, second = 2, curr;
+  if (n <= 2) return n;
+  for (let i = 3; i < n; i++) {
+    curr = first + second;
+    first = second;
+    second = curr;
+  }
+  return curr;
 }
 
 function powerOfThree(n) {
@@ -34,78 +49,45 @@ function powerOfThree(n) {
 }
 
 function reverseBits(n) {
-  let result = 0, count = 32;
+  let rev = 0, count = 32;
   while (count--) {
-    result *= 2;
-    result += n & 1;
-    n = n >> 1;
+    rev *= 2;
+    rev += n & 1;
+    n >> 1;
   }
-  return result;
-}
-
-function countPrimes(n) {
-  let count = 0, primes = [];
-  if (n <= 1) return count;
-  for (let i = 2; i < n; i++) {
-    if (primes[i] == undefined) {
-      primes[i] = true, count++;
-      for (let j = 2; j * i < n; j++) {
-        primes[i * j] = false;
-      }
-    } 
-  }
-  return count;
-}
-
-function hammingDistance(x, y) {
-  return (x ^ y).toString(2).replace(/0/g, '').length;
-}
-
-function reverseBits(n) {
-  let result = 0, count = 32;
-  while (count--) {
-    result *= 2;
-    result += n & 1;
-    n = n >> 1;
-  }
-  return result;
-}
-
-function maxSubarray(nums) {
-  let prev = 0, max = -Infinity;
-  for (let i = 0; i < nums.length; i++) {
-    prev = Math.max(nums[i] + prev, nums[i]);
-    max = Math.max(prev, max);
-  }
-  return max;
-}
-
-function pascalsTriangle(numRows) {
-  if (!numRows || numRows <= 0) return [];
-  let pascals = [[1]];
-  for (let i = 0; i < numRows; i++) {
-    const prevRow = pascals[pascals.length - 1];
-    const leftRow = [...prevRow, 0];
-    const rightRow = [0, ...prevRow];
-    const currRow = leftRow.map((r, i) => r + rightRow[i]);
-    pascals.push(currRow);
-  }
-  return pascals;
+  return rev;
 }
 
 function romanToInteger(s) {
-  let symb = {
-    ...
-  }
-  let count = 0;
+  const chars = {...}
+  let results = 0;
   for (let i = 0; i < s.length; i++) {
-    if (symb[s[i]] < symb[s[i + 1]]) {
-      count -= symb[s[i]];
+    if (chars[s[i]] < chars[s[i + 1]]) {
+      results -= chars[s[i]];
     } else {
-      count += symb[s[i]];
+      results += chars[s[i]];
     }
   }
-  return count;
+  return results;
+}
+
+function validParentheses(s) {
+  const chars = {};
+  const arr = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+      arr.push(s[i]);
+    } else {
+      if (arr[arr.length - 1] === chars[s[i]]) {
+        arr.pop();
+      } else return false;
+    }
+  }
+  return arr.length === 0;
+}
+
+function hammingDistance(x, y) {
+  return (x ^ y).toString(2).replace(/0/g, "").length;
 }
 
 function numberOf1Bits(n) {
@@ -117,152 +99,14 @@ function numberOf1Bits(n) {
   return count;
 }
 
-function houseRobber(nums) {
-  let before = prev = 0, curr;
-  for (let i = 0; i < nums.length; i++) {
-    curr = prev;
-    prev = Math.max(nums[i] + before, prev);
-    before = curr;
-  }
-  return prev;
-}
-
-function hammingDistance(x, y) {
-  return (x ^ y).toString(2).replace(/0/g, '').length;
-}
-
-function validParentheses(s) {
-  const chars = {...};
-  let arr = [];
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
-      arr.push(s[i]);
-    } else {
-      if (arr[arr.length - 1] == chars[s[i]]) {
-        arr.pop();
-      } else return false;
-    }
-  }
-  return arr.length === 0;
-}
-
 function reverseBits(n) {
   let res = 0, count = 32;
   while (count--) {
     res *= 2;
-    res += n & i;
+    res += n & 1;
     n >> 1;
   }
   return res;
-}
-function missingNumber(nums) {
-  let count = 0, total = 0;
-  for (let i = 0; i < nums.length; i++) {
-    count += nums[i];
-    total += i + 1;
-  }
-  return total - count;
-}
-
-function validParentheses(s) {
-  const map = {
-    ')': '(',
-    '}': '{',
-    ']': '['
-  };
-  let arr = [];
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
-      arr.push(s[i]);
-    } else {
-      if (arr[arr.length - 1] === map[s[i]]) {
-        arr.pop();
-      } else return false;
-    }
-  }
-  return arr.length === 0;
-}
-
-function powerOfThree(n) {
-  while (n > 1) {
-    n /= 3;
-  }
-  return n === 1;
-}
-
-function reverseBits(n) {
-  let result = 0, count = 32;
-  while (count--) {
-    result *= 2;
-    result += n & 1;
-    n = n >> 1;
-  }
-  return result;
-}
-
-function countPrimes(n) {
-  let count = 0, primes = [];
-  if (n <= 1) return count;
-  for (let i = 2; i < n; i++) {
-    if (primes[i] == undefined) {
-      primes[i] = true, count++;
-      for (let j = 2; j * i < n; j++) {
-        primes[i * j] = false;
-      }
-    } 
-  }
-  return count;
-}
-
-function hammingDistance(x, y) {
-  return (x ^ y).toString(2).replace(/0/g, '').length;
-}
-
-function reverseBits(n) {
-  let result = 0, count = 32;
-  while (count--) {
-    result *= 2;
-    result += n & 1;
-    n = n >> 1;
-  }
-  return result;
-}
-
-function maxSubarray(nums) {
-  let prev = 0, max = -Infinity;
-  for (let i = 0; i < nums.length; i++) {
-    prev = Math.max(nums[i] + prev, nums[i]);
-    max = Math.max(prev, max);
-  }
-  return max;
-}
-
-function pascalsTriangle(numRows) {
-  if (!numRows || numRows <= 0) return [];
-  let pascals = [[1]];
-  for (let i = 0; i < numRows; i++) {
-    const prevRow = pascals[pascals.length - 1];
-    const leftRow = [...prevRow, 0];
-    const rightRow = [0, ...prevRow];
-    const currRow = leftRow.map((r, i) => r + rightRow[i]);
-    pascals.push(currRow);
-  }
-  return pascals;
-}
-
-function romanToInteger(s) {
-  let symb = {
-    ...
-  }
-  let count = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (symb[s[i]] < symb[s[i + 1]]) {
-      count -= symb[s[i]];
-    } else {
-      count += symb[s[i]];
-    }
-  }
-  return count;
 }
 
 function numberOf1Bits(n) {
@@ -274,25 +118,24 @@ function numberOf1Bits(n) {
   return count;
 }
 
-function houseRobber(nums) {
-  let before = prev = 0, curr;
-  for (let i = 0; i < nums.length; i++) {
-    curr = prev;
-    prev = Math.max(nums[i] + before, prev);
-    before = curr;
+function pascalsTriangle(rows) {
+  if (!rows || rows <= 0) return [];
+  let pascal = [[1]];
+  for (let i = 1; i < rows; i++) {
+    let prev = pascal[pascal.length - 1];
+    let left = [...pascal, 0];
+    let right = [0, ...pascal];
+    let curr = left.map((r, i) => r + right[i]);
+    pascal.push(curr);
   }
-  return prev;
-}
-
-function hammingDistance(x, y) {
-  return (x ^ y).toString(2).replace(/0/g, '').length;
+  return pascal;
 }
 
 function validParentheses(s) {
   const chars = {...};
   let arr = [];
   for (let i = 0; i < s.length; i++) {
-    if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+    if (s[i] == "(" || s[i] == "{" || s[i] == "[") {
       arr.push(s[i]);
     } else {
       if (arr[arr.length - 1] == chars[s[i]]) {
@@ -303,12 +146,15 @@ function validParentheses(s) {
   return arr.length === 0;
 }
 
-function reverseBits(n) {
-  let res = 0, count = 32;
-  while (count--) {
-    res *= 2;
-    res += n & i;
-    n >> 1;
+function romanToInteger(s) {
+  const chars = {...};
+  const count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (chars[s[i]] < chars[s[i - 1]]) {
+      count -= chars[s[i]];
+    } else {
+      count += chars[s[i]];
+    }
   }
-  return res;
+  return count;
 }
