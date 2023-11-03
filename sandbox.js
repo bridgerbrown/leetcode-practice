9080bb670f64bc5815e184b9fdeb149ccb1e9911
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-11-03 12:56:22 -0700

    mastering all

--- sandbox.js
+++ sandbox.js
@@ -1,102 +1,118 @@
-function firstBadVersion(n, bad) {
-  return function(n) {
-    let left = 0, right = n;
-    while (right - left !== 1) {
-      const mid = Math.floor((left + right) / 2);
-      isBadVersion(mid) ? right = mid : left = mid;
-    }
-  }
-  return right;
+function reverseLL(head) {
+  let prev = temp = null, cur = head;
+  while (cur) {
+    temp = cur.next;
+    cur.next = prev;
+    prev = cur;
+    cur = temp;
+  };
+  return prev;
 }
 
-function climbStairs(n) {
-  if (n <= 2) return n;
-  let first = 1, second = 2, curr;
-  for (let i = 3; i < n; i++) {
-    curr = first + second;
-    first = second;
-    second = current;
+function stringToAtoi(s) {
+  return Math.max(-(2 ** 31), Math.min((2 ** 31 - 1), parseInt(s)) || 0);
+}
+
+function symmetricTree(root) {
+  if (!root) return true;
+  const DFS = (left, right) => {
+    if (!left && !right) return true;
+    if (left.val !== right.val || !left && right || left && !right) return false;
+    DFS(left.right, right.left), DFS(left.left, right.right);
   }
-  return curr;
+  return DFS(root.left, root.right);
+};
+
+function romanToInt(s) {
+  const chars = {...};
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (chars[s[i]] < chars[s[i + 1]]) {
+      count -= chars[s[i]];
+    } else {
+      count += chars[s[i]];
+    }
+  }
+  return count;
+}
+
+function missingNumber(nums) {
+  let sum = total = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    total += i + 1;
+  }
+  return total - sum;
 }
 
 function reverseLL(head) {
-  let prev = null, temp, curr = head;
-  while (curr.next) {
-    temp = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = temp;
+  let prev = temp = null, cur = head;
+  while (cur) {
+    temp = cur.next;
+    cur.next = prev;
+    prev = cur;
+    cur = temp;
   }
   return prev;
 }
 
 function validParentheses(s) {
-  const chars = {...};
-  let arr = [];
-  for (let i = 0; i < s.length; i++) {
+  const p = {...};
+  const arr = [];
+  for (let i = 0; i  < s.length; i ++) {
     if (s[i] == "(" || s[i] == "{" || s[i] == "[") {
       arr.push(s[i]);
     } else {
-      if (arr[arr.length - 1] == chars[s[i]]) {
+      if (arr[arr.length - 1] == p[s[i]]) {
         arr.pop();
-      } else return false;
+      } else {
+        return false;
+      }
     }
   }
   return arr.length === 0;
 }
 
-function powerOfThree(n) {
-  while(n > 1) {
-    n /= 3;
+var MinStack = function() {
+  this.minStack = [];
+  this.main = [];
+}
+MinStack.prototype.push = function(n) {
+  this.main.push(n);
+  if (!this.minStack.length || n <= this.minStack[this.minStack.length - 1]) {
+    this.minStack.push(n);
   }
-  return n === 1;
+}
+MinStack.prototype.pop = function() {
+  let val = this.main.pop();
+  if (val === this.minStack[this.minStack.length - 1]) {
+    this.minStack.pop();
+  }
+}
+MinStack.prototype.top = function() {
+  return this.main[this.main.length - 1];
+}
+MinStack.prototype.getMin = function() {
+  return this.minStack[this.minStack.length - 1];
 }
 
-function pascalsTriangle(r) {
-  if (r <= 0) return [];
-  let pascals = [[1]];
-  for (let i = 1; i < r; i++) {
-    const prev = pascals[pascals.length - 1];
-    const left = [...prev, 0];
-    const right = [0, ...prev];
-    const curr = left.map((r, i) => r + right[i]);
-    pascals.push(curr);
+function reverseBits(n) {
+  let rev = 0, count = 32;
+  while(count--) {
+    rev *= 2;
+    rev += i & 1;
+    n = n >> 1;
   }
-  return pascals;
+  return rev;
 }
 
-function numberOf1Bits(n) {
-  let count = 0;
-  while (n !== 0) {
-    count += n & 1;
-    n = n >>> 1;
+function climbStairs(n) {
+  if (n <= 2) return n;
+  let first = 1, second = 2, curr;
+  for (let i = 3; i  < n; i ++) {
+    curr = first + second;
+    first = second;
+    second = curr;
   }
-  return count;
-};
-
-function firstBadVersion(n, bad) {
-  return function(n) {
-    let left = 0, right = n;
-    while (right - left !== 1) {
-      const mid = Math.floor((left + right) / 2);
-      isBadVersion(mid) ? right = mid : left = mid;
-    }
-  }
-  return right;
-}
-
-function hammingDistance(x, y) {
-  return (x ^ y).toString(2).replace(/0/g, "").length;
-}
-
-function reverseLL(head) {
-  let prev = temp = null, curr = head;
-  while (curr) {
-    temp = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = temp;
-  }
-  return prev;
+  return second;
 }
