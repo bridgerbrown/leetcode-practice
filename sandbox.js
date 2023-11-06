c7aaa7cbb93bf4c2af026d716fae543c988c78f4
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-11-06 10:39:47 -0800

    daily top 50 practice

--- sandbox.js
+++ sandbox.js
@@ -1,118 +1,100 @@
 function reverseLL(head) {
-  let prev = temp = null, cur = head;
-  while (cur) {
-    temp = cur.next;
-    cur.next = prev;
-    prev = cur;
-    cur = temp;
-  };
+  let prev = temp = null, curr = head;
+  while(curr) {
+    temp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = temp;
+  }
   return prev;
 }
 
-function stringToAtoi(s) {
-  return Math.max(-(2 ** 31), Math.min((2 ** 31 - 1), parseInt(s)) || 0);
+function hammingDistance(x, y) {
+  return (x ^ y).toString(2).replace(/0/g, "").length;
 }
 
-function symmetricTree(root) {
-  if (!root) return true;
-  const DFS = (left, right) => {
-    if (!left && !right) return true;
-    if (left.val !== right.val || !left && right || left && !right) return false;
-    DFS(left.right, right.left), DFS(left.left, right.right);
+function reverseBits(n) {
+  let rev = 0, count = 32;
+  while (count--) {
+    rev *= 2;
+    rev += n & 1;
+    n = n >>> 1;
   }
-  return DFS(root.left, root.right);
-};
+  return rev;
+}
 
-function romanToInt(s) {
-  const chars = {...};
-  let count = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (chars[s[i]] < chars[s[i + 1]]) {
-      count -= chars[s[i]];
+function powerOfThree(n) {
+  while (n > 1) {
+    n /= 3;
+  }
+  return n === 1;
+}
+
+function strStr(haystack, needle) {
+  if (!needle.length) return 0;
+  return haystack.indexOf(needle);  
+} 
+
+function moveZeroes(nums) {
+  let diff = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 0) {
+      diff++
     } else {
-      count += chars[s[i]];
+      [nums[i - diff], nums[i]] = [nums[i], nums[i - diff]];
     }
   }
-  return count;
 }
 
-function missingNumber(nums) {
-  let sum = total = 0;
-  for (let i = 0; i < nums.length; i++) {
-    sum += nums[i];
-    total += i + 1;
+function mergeSortedArr(nums1, m, nums2, n) {
+  let i = m - 1, j = n - 1, k = m + n - 1;
+  while (i >= 0 && j >= 0) {
+    if (nums1.val > nums2.val) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
   }
-  return total - sum;
-}
-
-function reverseLL(head) {
-  let prev = temp = null, cur = head;
-  while (cur) {
-    temp = cur.next;
-    cur.next = prev;
-    prev = cur;
-    cur = temp;
+  while(j >= 0) {
+    nums1[k] = nums2[j];
+    j--, k--;
   }
-  return prev;
+  return nums1;
 }
 
 function validParentheses(s) {
-  const p = {...};
-  const arr = [];
-  for (let i = 0; i  < s.length; i ++) {
-    if (s[i] == "(" || s[i] == "{" || s[i] == "[") {
+  const map = {...};
+  if (!s.length) return true;
+  let arr = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == "(" || s[i] == "{" || s[i] == "[" ) {
       arr.push(s[i]);
     } else {
-      if (arr[arr.length - 1] == p[s[i]]) {
+      if (arr[arr.length - 1] == s[i]) {
         arr.pop();
-      } else {
-        return false;
-      }
+      } else return false;
     }
   }
   return arr.length === 0;
 }
 
-var MinStack = function() {
-  this.minStack = [];
-  this.main = [];
-}
-MinStack.prototype.push = function(n) {
-  this.main.push(n);
-  if (!this.minStack.length || n <= this.minStack[this.minStack.length - 1]) {
-    this.minStack.push(n);
+function validAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  const alph = new Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    alph[s.charCodeAt(i) - 97]++;
+    alph[t.charCodeAt(i) - 97]--;
   }
-}
-MinStack.prototype.pop = function() {
-  let val = this.main.pop();
-  if (val === this.minStack[this.minStack.length - 1]) {
-    this.minStack.pop();
-  }
-}
-MinStack.prototype.top = function() {
-  return this.main[this.main.length - 1];
-}
-MinStack.prototype.getMin = function() {
-  return this.minStack[this.minStack.length - 1];
+  return alph.every((char) => char === 0);
 }
 
-function reverseBits(n) {
-  let rev = 0, count = 32;
-  while(count--) {
-    rev *= 2;
-    rev += i & 1;
-    n = n >> 1;
+function validPalindromStr(s) {
+  const chars = s.toLowerCase().replace(/[^a-z0-9]/gi, "");
+  for (let i = 0, j = chars.length - 1; i <= j; i++, j--) {
+    if (chars.charAt(i) !== chars.charAt[j]) return false;
   }
-  return rev;
-}
-
-function climbStairs(n) {
-  if (n <= 2) return n;
-  let first = 1, second = 2, curr;
-  for (let i = 3; i  < n; i ++) {
-    curr = first + second;
-    first = second;
-    second = curr;
-  }
-  return second;
+  return true;
 }
