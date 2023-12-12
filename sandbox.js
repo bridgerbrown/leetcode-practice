cc6ea82d4da51f5a63740a0359fdc49b37867f96
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-12-11 19:19:33 -0800

    top daily

--- sandbox.js
+++ sandbox.js
@@ -1,66 +1,34 @@
-function mergeLLs(list1, list2) {
-  const merged = new Node();
-  const head = merged;
-  while (list1 && list2) {
-    if (list1.val < list2.val) {
-      merged.next = new Node(list1.val);
-      list1 = list1.next;
-    } else {
-      merged.next = new Node(list2.val);
-      list2 = list2.next;
-    }
-    merged = merged.next;
-  }
-  merged.next = list1 || list2;
-  return head.next;
-}
-
-function convertsortedarrbst(nums) {
-  const BST = (left, right) => {
-    if (left > right) return null;
-    const mid = Math.floor((left + right) / 2);
-    const current = new Node(nums[mid]);
-    current.left = BST(left, mid - 1);
-    current.right = BST(mid + 1, right);
-    return current;
-  }
-  return BST(0, nums.length - 1);
-}
-
-function arrayintersection(nums1, nums2) {
-  const freq = {};
-  const intersection = [];
-  for (let num of nums1) {
-    freq[num] = (...freq[num] || 0) + 1;
-  }
-  for (let num of nums2) {
-    if (freq[num] > 0) {
-      intersection.push(num);
-      freq[num]--;
-    }
-  }
-  return intersection;
-}
-
-function firstuniquecharstr(s) {
+function firstUniqueInStr(s) {
   const freq = {};
   for (const char of s) {
     freq[char] = (freq[char] || 0) + 1;
   }
-  for (let i = 0; i < s.length; i ++) {
+  for (let i = 0; i < s.length; i++) {
     if (freq[s[i]] === 1) return i;
   }
   return -1;
 }
 
-function btleveltraversal(root) {
-  const levels = [];
-  if (!root) return levels;
-  const DFS = (node, level) => {
-    levels[level] = [...(levels[level] || []), node.val];
-    if (node.left) DFS(node.left, level + 1);
-    if (node.right) DFS(node.right, level + 1);
+function sortedArrToBST(nums) {
+  function BST(left, right) {
+    if (left > right) return null;
+    const mid = Math.floor((left + right) / 2);
+    const curr = new Node(nums[mid]);
+    curr.left = BST(left, mid - 1);
+    curr.right = BST(mid + 1, right);
+    return curr;
   }
-  DFS(root, 0);
-  return levels;
+  return BST(0, nums.length - 1);
+}
+
+function unique(nums) {
+  const set = new Set();
+  for (let num of nums) set.has(num) ? set.delete(num) : set.add(num);
+  return set.values().next().value;
+}
+
+function maxDepth(root) {
+  if (!root) return null;
+  let max = Math.max(maxDepth(root.left), maxDepth(root.right));
+  return max + 1;
 }
