687e3ce0bc17d907b58ec2d0fcc3bcc33c8d95bd
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2024-01-19 12:47:53 -0800

    neetcode

--- neetcode-sandbox.js
+++ neetcode-sandbox.js
@@ -1,120 +1,19 @@
-function groupAnagrams(strs) {
-  let obj = {};
-  for (let str of strs) {
-    let letters = str.split("").sort().join("");
-    obj[letters] ? obj[letters].push(str) : obj[letters] = [str];
-  }
-  return Object.values(obj);
-}
-
-function permInStr(s1, s2) {
-  if (s1.length > s2.length) return false;
-  let needed = {};
-  for (let char of s1) {
-    needed[char] = (needed[char] || 0) + 1;
-  }
-
-  let l = 0, r = 0, requiredLength = s1.length;
-  while (r < s2.length) {
-    if (needed[s2[r]] > 0) requiredLength--;
-    needed[s2[r]]--;
-    r++;
-
-    if (requiredLength === 0) return true;
-
-    if (r - l === s1.length) {
-      if (needed[s2[l]] >= 0) requiredLength++;
-      needed[s2[l]]++;
-      l++;
-    }
-  }
-  return false;
-}
-
-function permInStr(s1, s2) {
-  if (s1.length > s2.length) return false;
-  let needed = {};
-  for (let char of s1) {
-    needed[char] = (needed[char] || 0) + 1;
-  }
-  
-  let l = 0, r = 0, requiredLength = s1.length;
-  while (r < s2.length) {
-    if (needed[s2[r]] > 0) requiredLength--;
-    needed[s2[r]]--;
-    r++;
-
-    if (requiredLength === 0) return true;
-
-    if (r - l === s1.length) {
-      if (needed[s2[l]] >= 0) requiredLength++;
-      needed[s2[l]]++;
-      l++;
-    }
-  }
-  return false
-}
-
-function longestRepeatingReplacement(s, k) {
-  let count = 0;
-  let freq = new Map();
-  let l = 0;
-
+function longestWoRepeating(s) {
+  let left = 0;
+  let set = new Set();
+  let max = 0;
   for (let r = 0; r < s.length; r++) {
-    let win = r - l + 1;
-    freq.set(s[r], (freq.get(s[r]) || 0) + 1);
-
-    if ((win - Math.max(...freq.values())) > k) {
-      freq.set(s[l], freq.get(s[l]) - 1);
-      l++;
+    if (set.has(s[i])) {
+      left++;
+      set.delete(s[l]);
     }
-
-    win = r - l + 1;
-    count = Math.max(count, win);
+    set.add(s[l]);
+    max = Math.max(max, left - r + 1);
   }
-  return count;
+  return max;
 }
 
-function longestRepeatingReplacement(s, k) {
-  let count = 0;
-  let freq = new Map();
-  let l = 0;
-
-  for (let r = 0; r < s.length; r++) {
-    let win = r - l + 1;
-    freq.set(s[r], (freq.get(s[r]) || 0) + 1);
-
-    if((win - Math.max(...freq.values())) > k) {
-      freq.set(s[l], freq.get(s[l]) - 1);
-      l++;
-    }
-
-    win = r - l + 1;
-    count = Math.max(count, win);
-  }
-  return count;
-}
-
-function slidingWMax(nums, k) {
-  const indices = [];
-  const res = [];
-
-  for (let i = 0; i < nums.length; i++) {
-    while (indices.length && nums[indices[indices.length - 1]] <= nums[i]) {
-      indices.pop();  
-    }
-    indices.push(i);
-    if (indices[0] === i - k) {
-      indices.shift();
-    }
-    if (i >= k - 1) {
-      res.push(nums[indices[0]]);
-    }
-  }
-  return res;
-}
-
-function slidingWMax(nums, k) {
+function slidingWinMax(nums, k) {
   const q = [];
   const res = [];
   for (let i = 0; i < nums.length; i++) {
@@ -122,41 +21,50 @@ function slidingWMax(nums, k) {
       q.pop();
     }
     q.push(i);
-    if (q[0] === i - k) {
-      q.shift();
-    }
-    if (i >= k - 1) {
-      res.push(nums[q[0]]);
-    }
+    if (q[0] === i - k) q.shift();
+    if (i >= k - 1) res.push(nums[q[0]]);
   }
   return res;
 }
 
-function minWind(s, t) {
-  let min = "", l = 0, r = -1;
-  let freq = {};
-  t.split('').forEach(e => {
-    freq[e] ? freq[e] = freq[e] + 1 : freq[e] = 1;
-  });
-  let uniqueCount = Object.keys(freq).length;
-  while (r <= s.length) {
-    if (uniqueCount == 0) {
-      let curr = s[l];
-      if (freq[curr] != null) freq[curr]++;
-      if (freq[curr] > 0) uniqueCount++;
+function permInStr(s1, s2) {
+  let l = 0;
+  let r = 0;
+  let requiredLength = s1.length;
+  let freq = new Map();
+  for (let char of s1) {
+    freq[char] = (freq[char] || 0) + 1;
+  }
+  while (r <= s2.length){
+    if (freq[s2[r]] > 0) requiredLength--;
+    freq[s2[r]]--;
+    r++;
 
-      let temp = s.substring(l, r + 1);
+    if (requiredLength === 0) return true;
 
-      if (min == "") min = temp;
-      else min = min.length < temp.length ? min : temp;
-    } else {
-      r++;
-      let curr = s[r];
-      if (freq[curr] != null) freq[curr]--;
-      if (freq[curr] == 0) uniqueCount--;
+    if (r - l === s1.length) {
+      if (freq[s2[l]] >= 0) requiredLength++;
+      freq[s2[l]]++;
+      l++;
     }
   }
-  return min;
+}
+
+function longestRepCharRep(s, k) {
+  let count = 0;
+  let freq = new Map();
+  let l = 0;
+  for (let r = 0; r < s.length; r++) {
+    let win = r - l + 1;
+    freq.set(s[r], (freq.get[s[r]] || 0) + 1);
+    if ((win - Math.max(...freq.values())) > k) {
+      freq.set(s[l], freq.get(s[l]) - 1);
+      l++;
+    }
+    win = r - l + 1;
+    count = Math.max(count, win);
+  }
+  return count;
 }
 
 function minWind(s, t) {
@@ -164,17 +72,16 @@ function minWind(s, t) {
   let freq = {};
   t.split('').forEach(e => {
     freq[e] = (freq[e] || 0) + 1;
-  })
+  });
+  let unique = Object.keys(freq).length;
 
-  let uniqueCount = Object.keys(freq).length;
   while (r <= s.length) {
-    if (uniqueCount == 0) {
+    if (unique == 0) {
       let curr = s[l];
       if (freq[curr] != null) freq[curr]++;
-      if (freq[curr] > 0) uniqueCount++;
+      if (freq[curr] > 0) unique++;
 
       let temp = s.substring(l, r + 1);
-
       if (min == "") min = temp;
       else min = min.length < temp.length ? min : temp;
       l++;
@@ -182,7 +89,7 @@ function minWind(s, t) {
       r++;
       let curr = s[r];
       if (freq[curr] != null) freq[curr]--;
-      if (freq[curr] == 0) uniqueCount--;
+      if (freq[curr] == 0) unique--;
     }
   }
   return min;
