b5a7a3955f039433396660b2aca5fa8ee3b56f9c
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2024-02-01 13:46:34 -0800

    two pointers, stack, and all practice

--- neetcode-150/stack.js
+++ neetcode-150/stack.js
@@ -20,6 +20,9 @@ Output: 9
 Explanation: ((2 + 1) * 3) = 9
 */
 
+// Reverse Polish Notation is just postfix notation, where operators follow their operands:
+// 3 4 + for 3 + 4
+
 function evalRPN(tokens) {
   let stack = [];
   let ops = {
@@ -38,4 +41,81 @@ function evalRPN(tokens) {
     }
   }
   return stack.pop();
+}
+
+/*
+#2: Generate Parentheses
+
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+*/
+
+var generateParenthesis = (n) => dfs(n);
+
+const dfs = (n, combos = [], open = 0, close = 0, path = []) => {
+    const isBaseCase = (path.length === (n * 2));
+    if (isBaseCase) {
+        combos.push(path.join(''));
+
+        return combos; 
+    }
+
+    const isOpen = open < n;
+    if (isOpen) backTrackOpen(n, combos, open, close, path);
+
+    const isClose = close < open;
+    if (isClose) backTrackClose(n, combos, open, close, path);
+
+    return combos;
+}
+
+const backTrackOpen = (n, combos, open, close, path) => {
+    path.push('('); 
+        dfs(n, combos, (open + 1), close, path);
+    path.pop();
+}
+
+const backTrackClose = (n, combos, open, close, path) => {
+    path.push(')');
+        dfs(n, combos, open, (close + 1), path);
+    path.pop();
+}
+// Time O(2^N) | Space O(2^N)
+
+/*
+#3: Daily Temperatures
+
+Given an array of integers temperatures represents the daily temperatures, 
+return an array answer such that answer[i] is the number of days you have to wait after 
+the ith day to get a warmer temperature. If there is no future day for which this is 
+possible, keep answer[i] == 0 instead.
+
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+*/
+
+function dailyTemperatures(temps, stack = []) { // stack is in decreasing order (non increasing)
+  const days = Array(temps.length).fill(0);
+
+  for (let day = 0; day < temps.length; day++) {
+    while (canShrink(stack, temps, day)) {
+      const prevColdDay = stack.pop();
+      const daysToWait = (day - prevColdDay);
+      days[prevColdDay] = daysToWait;
+    } 
+    stack.push(day);
+  }
+  return days;
+}
+const canShrink = (stack, temps, day) => {
+  const prevDay = stack[stack.length - 1];
+  const [prevTemp, currTemp] = [temps[prevDay], temps[day]];
+  const isWarmer = prevTemp < currTemp;
+  return stack.length && isWarmer;
 }
--- neetcode-sandbox.js
+++ neetcode-sandbox.js
@@ -1,49 +1,114 @@
+function evalRPN(tokens) {
+  const stack = [];
+  const ops = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b >= 0 ? Math.floor(a / b) : Math.ceil(a / b)
+  };
+  for (let t of tokens) {
+    if (ops[t]) {
+      let top = stack.pop();
+      let second = stack.pop();
+      stack.push(ops[t](second, top));
+    } else {
+      stack.push(Number(t));
+    }
+  }
+  return stack.pop();
+}
+
+function generateParen(n) {
+  const DFS = (n, combos = [], open = 0, close = 0, path = []) => {
+    const baseCase = (path.length === (n * 2));
+    if (baseCase) {
+      combos.push(path.join(''));
+      return combos;
+    }
+
+    const isOpen = open < n;
+    if (isOpen) backTrackOpen(n, combos, open, close, path);
+    const isClose = close < open;
+    if (isClose) backTrackClose(n, combos, open, close, path);
+    
+    return combos;
+  }
+  DFS(n);
+}
+const backTrackOpen = (n, combos, open, close, path) => {
+  path.push('(');
+  DFS(n, combos, (open + 1), close, path);
+  path.pop();
+}
+const backTrackClose = (n, combos, open, close, path) => {
+  path.push(')');
+  DFS(n, combos, open, (close + 1), path);
+  path.pop();
+}
+
 function threeSum(nums) {
   const res = [];
   nums.sort((a, b) => a - b);
-  for (let i = 0; i < nums.length; i++) {
-    const a = nums[i];
-    if (a > 0) break;
-    if (i > 0 && a === nums[i - 1]) continue;
 
-    let l = i + 1;
-    let r = nums.length - 1;
+  for (let i = 0; i < nums.length; i++) {
+    let l = i + 1, r = nums.length - 1;
+    const curr = nums[i];
+    if (curr > 0) break;
+    if (i > 0 && curr === nums[i - 1]) continue;
+
     while (l < r) {
-      const threeSum = a + nums[l] + nums[r];
-      if (threeSum > 0) {
-        r--;
-      } else if (threeSum < 0) {
+      const sum = curr + nums[l] + nums[r];
+      if (sum < 0) {
         l++;
+      } else if (sum > 0) {
+        r--;
       } else {
-        res.push([a, nums[l], nums[r]]);
+        res.push([curr, nums[l], nums[r]]);
         l++, r--;
         while (nums[l] === nums[l - 1] && l < r) l++;
       }
     }
   }
-}
-
-function trap(height) {
-  let l = 0, r = height.length - 1;
-  const lMax = height[l];
-  const rMax = height[r];
-  let res = 0;
-  while (l < r) {
-    if (lMax < rMax) {
-      l++;
-      lMax = Math.max(lMax, height[l]);
-      res += lMax - height[l];
-    } else {
-      r--;
-      rMax = Math.max(rMax, height[r]);
-      res += rMax - height[r];
-    }
-  }
   return res;
 }
 
-function maxArea(height) {
-  const res = 0;
+function twoSumII(nums, target) {
+  let l = 0, r = nums.length - 1;
+  while (l < r) {
+    const sum = nums[l] + nums[r];
+    if (sum > target) {
+      r--;
+    } else if (sum < target) {
+      l++;
+    } else {
+      return [l + 1, r + 1];
+    }
+  }
+  return [-1, -1];
+}
+
+function longestRepChRep(s, k) {
+  const freq = new Map();
+  let max = 0;
+  let l = 0, r = 0;
+  while (r < s.length) {
+    let wind = r - l + 1;
+
+    freq.set(s[r], (freq.get(s[r]) || 0) + 1);
+
+    if ((wind - Math.max(...freq.values())) > k) {
+      freq.set(s[l], freq.get(s[l]) - 1);
+      l++;
+    }
+    wind = r - l + 1;
+    max = Math.max(max, wind);
+    r++;
+  }
+  return max;
+}
+
+function containerWithMostWater(height) {
+  const res = 0; 
   let l = 0, r = height.length - 1;
   while (l < r) {
     const area = (r - l) * Math.min(height[l], height[r]);
@@ -57,135 +122,25 @@ function maxArea(height) {
   return res;
 }
 
-function topFreqElem(nums, k) {
+function groupAnagrams(strs) {
+  let obj = {};
+  for (let str of strs) {
+    let chars = str.split("").sort().join("");
+    obj[chars] ? obj[chars].push(str) : obj[chars] = [str];
+  }
+  return Object.values(obj);
+}
+
+function longestRepChRep(s, k) {
+  const count = 0;
   const freq = new Map();
-  const bucket = [];
-  const res = [];
-  for (let num of nums) {
-    freq.set(freq.get(num) || 0) + 1;
-  }
-  for (let [num, f] of freq) {
-    bucket[f] = (bucket[f] || new Set()).add(num);
-  }
-  for (let i = bucket.length - 1; i >= 0; i--) {
-    if (bucket[i]) res.push(...bucket[i]);
-    if (res.length === k) break;
-  }
-  return res;
-}
-
-function twoSumII(nums, target) {
-  let res = [];
-  let l = 0, r = nums.length - 1;
-  while (l < r) {
-    const sum = nums[l] + nums[r];
-    if (sum === target) return [l + 1, r + 1];
-    if (sum < target) {
-      l++;
-    } else {
-      r--;
-    }
-  }
-  return [-1, -1];
-}
-
-function trap(height) {
-  let l = 0, r = height.length - 1;
-  const lMax = height[l];
-  const rMax = height[r];
-  let count = 0;
-  while (l < r) {
-    if (lMax < rMax) {
-      l++;
-      lMax = Math.max(lMax, height[l]);
-      count += lMax - height[l];
-    } else {
-      r--;
-      rMax = Math.max(rMax, height[r]);
-      count += rMax - height[r];
-    }
-  }
-  return count;
-}
-
-function threeSum(nums) {
-  const res = [];
-  nums.sort((a, b) => a - b);
-  for (let i = 0; i < nums.length; i++) {
-    let curr = nums[i];
-    if (curr > 0) break;
-    if (i > 0 && curr === nums[i - 1]) continue;
-
-    let l = i + 1, r = nums.length - 1;
-    while (l < r) {
-      const sum = curr + nums[l] + nums[r];
-      if (sum < 0) {
-        l++;
-      } else if (sum > 0) {
-        r--;
-      } else {
-        res.push([curr, nums[l], nums[r]]);
-        while (nums[l] === nums[l - 1] && l < r) l++;        
-      }
-    }
-  }
-  return res;
-}
-
-function topKFreq(nums) {
-  const freq = new Map();
-  const bucket = [];
-  const res = [];
-  for (let num of nums) {
-    freq.set(freq.get(num) || 0) + 1;
-  }
-  for (let [num, f] of freq) {
-    bucket[f] = (bucket[f] || new Set()).add(num);
-  }
-  for (let i = bucket.length - 1; i >= 0; i--) {
-    if (bucket[i]) res.push(...bucket(i));
-    if (res.length === k) break;
-  }
-  return res;
-}
-
-function productExceptSelf(nums) {
-  const res = [];
-  let lMult = 1, rMult = 1;
-  for (let i = nums.length - 1; i > 0; i--) {
-    res[i] = rMult;
-    rMult *= nums[i];
-  }
-  for (let j = 0; j < nums.length; j++) {
-    res[i] *= lMult;
-    lMult *= nums[j];
-  }
-  return res;
-}
-
-function twoSumII(nums, target) {
-  let l = 0, r = nums.length - 1;
-  while (l < r) {
-    const sum = nums[l] + nums[r];
-    if (sum === target) return [l + 1, r + 1];
-    if (sum < target) {
-      l++;
-    } else {
-      r--;
-    }
-  }
-  return [-1, -1];
-}
-
-function longestRepChar(s, k) {
-  const freq = new Map();
-  let count = 0;
-  for (let r = 0; r < s.length; r++) {
+  let l = 0, r = 0;
+  while (r < s.length) {
     let wind = r - l + 1;
     freq.set(s[r], (freq.get(s[r]) || 0) + 1);
-
+    
     if ((wind - Math.max(...freq.values())) > k) {
-      freq.set(s[l], freq.get(s[l]) - 1);
+      freq.set(s[l], (freq.get[s[l]])--);
       l++;
     }
     wind = r - l + 1;
@@ -197,73 +152,73 @@ function longestRepChar(s, k) {
 function slideWindMax(nums, k) {
   const idx = [];
   const res = [];
-  let l = 0, r = 0;
-  while (r < nums.length - 1) {
-    while (idx.length && nums[idx[idx.length - 1]] < nums[i]) {
+
+  let l = 0;
+  for (let r = 0; r < r.length; r++) {
+    while (idx.length && nums[idx[idx.length - 1]] < nums[r]) {
       idx.pop();
     }
-    idx.push(i);
+    idx.push(r);
     if (l > idx[0]) idx.shift();
     if (r - l + 1 === k) {
       res.push(nums[idx[0]]);
       l++;
     }
-    r++;
   }
   return res;
 }
 
-function longestSubWoRepCh(s) {
-  let freq = new Set();
-  let l = 0;
+function containerWithMostWater(height) {
+  let l = 0, r = height.length - 1;
   let max = 0;
-  for (let r = 0; r < s.length; r++) {
-    while (set.has(s[r])) {
-      set.delete(s[l]);
+  while (l < r) {
+    const area = (r - l) * Math.min(height[l], height[r]);
+    max = Math.max(max, area);
+    if (height[l] > height[r]) {
+      r--;
+    } else {
       l++;
-    } 
-    set.add(s[r]);
-    max = Math.max(max, r - l + 1);
+    }
   }
   return max;
 }
 
-function containerMostWater(height) {
-  let l = 0, r = height.length - 1;
-  const res = 0;
-  while (l < r) {
-    const square = r - l + 1 * (Math.min(height[l] , height[r]));
-    res = Math.max(res, area);
-    if (height[l] < height[r]) {
+function longestWoRepCh(s) {
+  const freq = new Set();
+  let l = 0, r = 0;
+  let max = 0;
+  while (r < s.length) {
+    if (set.has(s[r])) {
+      set.delete(s[l]);
       l++;
-    } else {
-      r--;
     }
+    set.add(s[r]);
+    max = Math.max(max, r - l + 1);
+    r++;
   }
-  return res;
+  return max;
 }
 
-function threeSum(nums) {
-  const res = [];
-  nums.sort((a, b) => a - b);
-  for (let i = 0; i < nums.length; i++) {
-    const curr = nums[i];
-    if (curr > 0) break;
-    if (i > 0 && curr === nums[i - 1]) continue;
-    let l = i + 1;
-    let r = nums.length - 1;
-    while (l < r) {
-      const sum = curr + nums[l] + nums[r];
-      if (sum > 0) {
-        r--;
-      } else if (sum < 0) {
-        l++;
-      } else {
-        res.push([curr, nums[l], nums[r]]);
-        l++, r--;
-        while (nums[l] === nums[l - 1] && l < r) l++;
-      }
+function minWindSub(s, t) {
+  let freq = {};
+  let min = "";
+  for (let ch of t) {
+    freq[ch] = (freq[ch] || 0) + 1;
+  }
+  let l = 0, r = -1, unique = Object.keys(freq).length;
+  while (r < s.length) {
+    if (unique.length === 0) {
+      if (freq[s[l]] !== undefined) freq[s[l]]++;
+      if (freq[s[l]] > 0) unique++;
+
+      let temp = s.substring(l, r + 1);
+      temp.length < min.length ? min = temp : min = min;
+      l++;
+    } else {
+      r++;
+      if (freq[s[r]] !== undefined) freq[s[r]]--;
+      if (freq[s[r]] > 0) unique--;
     }
   }
-  return res;
+  return min;
 }
